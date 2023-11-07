96fe867b1348bad292bb9e4abf3fb35e64221873
minhee jung <minheejung18@gmail.com>
2023-11-07 14:45:24 +0900

    first repository is made

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import TodoList from '@/components/TodoList'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <TodoList></TodoList>
+  )
+}
